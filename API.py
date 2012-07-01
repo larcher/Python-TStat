060478c60d458030d293e400e339f0f30a1c2a25
Larry Archer <larcher@spoon.cx>
2012-07-01 01:24:49 -0500

    Ignore vim files. :retab to convert to spaces.

--- API.py
+++ API.py
@@ -39,13 +39,13 @@
 # are instances of APIEntry.  (See APIv1 for an example.)
 #
 # Example APIEntry:
-#		'fmode': APIEntry(
-#			[('/tstat/fmode', 'fmode'), ('/tstat', 'fmode')],
-#			[('/tstat/fmode', 'fmode')],
-#			{'0': 'Auto', '1': '??', '2':'On'} #TODO: Check these values
-#		)
+#       'fmode': APIEntry(
+#           [('/tstat/fmode', 'fmode'), ('/tstat', 'fmode')],
+#           [('/tstat/fmode', 'fmode')],
+#           {'0': 'Auto', '1': '??', '2':'On'} #TODO: Check these values
+#       )
 # APIEntry has three members:
-# 	getters:  A list of tuples where the first entry is a URL on the thermostat
+#   getters:  A list of tuples where the first entry is a URL on the thermostat
 #             and the second entry is the JSON key used to retrieve the piece 
 #             of data.  In the above example, fmode can be retrieved either 
 #             from /tstat/fmode using key fmode, or from /tstat using key 
@@ -71,136 +71,136 @@
 
 class APIEntry:
 	def __init__(self, getters, setters, valueMap=None, usesJson=True):
-		self.getters = getters
-		self.setters = setters
-		self.valueMap = valueMap
-		self.usesJson = usesJson
+        self.getters = getters
+        self.setters = setters
+        self.valueMap = valueMap
+        self.usesJson = usesJson
 
 class API:
-	models = []
-	successStrings = []
-	entries = None
+    models = []
+    successStrings = []
+    entries = None
 
-	def __getitem__(self, item):
-		return self.entries[item]
+    def __getitem__(self, item):
+        return self.entries[item]
 
-	def has_key(self, key):
-		return self.entries.has_key(key)
+    def has_key(self, key):
+        return self.entries.has_key(key)
 
-	entries = {
-		'model': APIEntry(
-			[('/tstat/model', 'model')],
-			[]
-		)
-	}
+    entries = {
+        'model': APIEntry(
+            [('/tstat/model', 'model')],
+            []
+        )
+    }
 
 class API_CT50v109(API):
-	models = ['CT50 V1.09']
-	successStrings = [
-						"Tstat Command Processed",
-						"Cloud updates have been suspended till reboot",
-						"Cloud updates activated"
-	]
-	entries = {
-		'fmode': APIEntry(
-			[('/tstat/fmode', 'fmode'), ('/tstat', 'fmode')],
-			[('/tstat/fmode', 'fmode')],
-			{0: 'Auto', 1: '??', 2:'On'} #TODO: Check these values
-		),
-		'tmode': APIEntry(
-			[('/tstat/tmode', 'tmode'), ('/tstat', 'tmode')],
-			[('/tstat/tmode', 'tmode')],
-			{0: 'Off', 1: 'On'} #TODO: Check these values
-		),
-		'temp': APIEntry(
-			[('/tstat', 'temp'), ('/temp', 'temp')],
-			[]
-		),
-		'override': APIEntry(
-			[('/tstat', 'override'), ('/tstat/info', 'override'), ('/tstat/override', 'override')],
-			[],
-			{0: False, 1: True}
-		),
-		'hold': APIEntry(
-			[('/tstat', 'hold'), ('/tstat/info', 'hold'), ('/tstat/hold', 'hold')],
-			[('/tstat/hold', 'hold')],
-			{0: False, 1: True}
-		),
-		't_heat': APIEntry(
-			[('/tstat/info', 't_heat'), ('/tstat/ttemp', 't_heat')],
-			[('/tstat/ttemp', 't_heat')]
-		),
-		't_cool': APIEntry(
-			[('/tstat/info', 't_cool'), ('/tstat/ttemp', 't_cool')],
-			[('/tstat/ttemp', 't_cool')]
-		),
-		'tstate': APIEntry(
-			[('/tstat', 'tstate')],
-			[],
-			{0: 'Off', 1: 'On'}
-		),
-		'fstate': APIEntry(
-			[('/tstat', 'fstate')],
-			[],
-			{0: 'Off', 1: 'On'}
-		),
-		'day': APIEntry(
-			[('/tstat', 'time/day')],
-			[],
-			{0: 'Monday', 1: 'Tuesday', 2: 'Wednesday', 3: 'Thursday', 4: 'Friday', 5: 'Saturday', 6: 'Sunday'}
-		),
-		'hour': APIEntry(
-			[('/tstat', 'time/hour')],
-			[]
-		),
-		'minute': APIEntry(
-			#[('/tstat', 'time/minute'), ('/tstat/time/minute', 'day')],
-			[('/tstat', 'time/minute')],
-			[]
-		),
-		'today_heat_runtime': APIEntry(
-			[('/tstat/datalog', 'today/heat_runtime')],
-			[]
-		),
-		'today_cool_runtime': APIEntry(
-			[('/tstat/datalog', 'today/cool_runtime')],
-			[]
-		),
-		'yesterday_heat_runtime': APIEntry(
-			[('/tstat/datalog', 'yesterday/heat_runtime')],
-			[]
-		),
-		'yesterday_cool_runtime': APIEntry(
-			[('/tstat/datalog', 'yesterday/cool_runtime')],
-			[]
-		),
-		'errstatus': APIEntry(
-			[('/tstat/errstatus', 'errstatus')],
-			[],
-			{0: 'OK'}
-		),
-		'model': APIEntry(
-			[('/tstat/model', 'model')],
-			[]
-		),
-		'power': APIEntry(
-			[('/tstat/power', 'power')],
-			[('/tstat/power', 'power')]
-		),
-		'cloud_mode': APIEntry(
-			[],
-			[('/cloud/mode', 'command')],
-			usesJson=False
-		)
-		#'eventlog': #TODO
-	}
+    models = ['CT50 V1.09']
+    successStrings = [
+                        "Tstat Command Processed",
+                        "Cloud updates have been suspended till reboot",
+                        "Cloud updates activated"
+    ]
+    entries = {
+        'fmode': APIEntry(
+            [('/tstat/fmode', 'fmode'), ('/tstat', 'fmode')],
+            [('/tstat/fmode', 'fmode')],
+            {0: 'Auto', 1: '??', 2:'On'} #TODO: Check these values
+        ),
+        'tmode': APIEntry(
+            [('/tstat/tmode', 'tmode'), ('/tstat', 'tmode')],
+            [('/tstat/tmode', 'tmode')],
+            {0: 'Off', 1: 'On'} #TODO: Check these values
+        ),
+        'temp': APIEntry(
+            [('/tstat', 'temp'), ('/temp', 'temp')],
+            []
+        ),
+        'override': APIEntry(
+            [('/tstat', 'override'), ('/tstat/info', 'override'), ('/tstat/override', 'override')],
+            [],
+            {0: False, 1: True}
+        ),
+        'hold': APIEntry(
+            [('/tstat', 'hold'), ('/tstat/info', 'hold'), ('/tstat/hold', 'hold')],
+            [('/tstat/hold', 'hold')],
+            {0: False, 1: True}
+        ),
+        't_heat': APIEntry(
+            [('/tstat/info', 't_heat'), ('/tstat/ttemp', 't_heat')],
+            [('/tstat/ttemp', 't_heat')]
+        ),
+        't_cool': APIEntry(
+            [('/tstat/info', 't_cool'), ('/tstat/ttemp', 't_cool')],
+            [('/tstat/ttemp', 't_cool')]
+        ),
+        'tstate': APIEntry(
+            [('/tstat', 'tstate')],
+            [],
+            {0: 'Off', 1: 'On'}
+        ),
+        'fstate': APIEntry(
+            [('/tstat', 'fstate')],
+            [],
+            {0: 'Off', 1: 'On'}
+        ),
+        'day': APIEntry(
+            [('/tstat', 'time/day')],
+            [],
+            {0: 'Monday', 1: 'Tuesday', 2: 'Wednesday', 3: 'Thursday', 4: 'Friday', 5: 'Saturday', 6: 'Sunday'}
+        ),
+        'hour': APIEntry(
+            [('/tstat', 'time/hour')],
+            []
+        ),
+        'minute': APIEntry(
+            #[('/tstat', 'time/minute'), ('/tstat/time/minute', 'day')],
+            [('/tstat', 'time/minute')],
+            []
+        ),
+        'today_heat_runtime': APIEntry(
+            [('/tstat/datalog', 'today/heat_runtime')],
+            []
+        ),
+        'today_cool_runtime': APIEntry(
+            [('/tstat/datalog', 'today/cool_runtime')],
+            []
+        ),
+        'yesterday_heat_runtime': APIEntry(
+            [('/tstat/datalog', 'yesterday/heat_runtime')],
+            []
+        ),
+        'yesterday_cool_runtime': APIEntry(
+            [('/tstat/datalog', 'yesterday/cool_runtime')],
+            []
+        ),
+        'errstatus': APIEntry(
+            [('/tstat/errstatus', 'errstatus')],
+            [],
+            {0: 'OK'}
+        ),
+        'model': APIEntry(
+            [('/tstat/model', 'model')],
+            []
+        ),
+        'power': APIEntry(
+            [('/tstat/power', 'power')],
+            [('/tstat/power', 'power')]
+        ),
+        'cloud_mode': APIEntry(
+            [],
+            [('/cloud/mode', 'command')],
+            usesJson=False
+        )
+        #'eventlog': #TODO
+    }
 
 class API_CT30v192(API_CT50v109):
-		models = ['CT30 V1.92']
+        models = ['CT30 V1.92']
 
 APIs = [API_CT50v109, API_CT30v192]
 
 def getAPI(model):
-	for api in APIs:
-		if model in api.models:
-			return api()
+    for api in APIs:
+        if model in api.models:
+            return api()
